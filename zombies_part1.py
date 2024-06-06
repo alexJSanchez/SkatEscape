e957b9def01ebbd89049f91c0f46224bfd7bcf74
alex sanchez <alexjsanchez1020@gmail.com>
2024-06-05 21:24:04 -0400

    display game title

--- zombies_part1.py
+++ zombies_part1.py
@@ -1,24 +1,18 @@
 import random
 import time
 
- # constants for grid dimensions
-GRID_WIDTH = 20   
+# Constants for grid dimensions
+GRID_WIDTH = 20
 GRID_HEIGHT = 10
 
-# constants for number of zombies an quicksand 
-ZOMBIE_COUNT = 4 
+# Constants for number of zombies and quicksandd
+ZOMBIE_COUNT = 4
 QUICKSAND_COUNT = 8
 
-# Set area of text and print title on screen
+# Set area of text and print title on screen.
 
 def initialize_screen():
-    # Display the game title 
-    print(".........OFFICERS")
+    # Display the game title
+    print("......ZOMBIES")
     print()
 
-
-# Print area of the ranch within which you and zombies can move
-def get_random_coordinate(limit):
-        # generate a random coordinate with a given limit
-        return random.randint(1,limit)
-    
